0ba06e48a133e7d7ffc5b86bee95b646b5ccd8af
Joseph Scheuhammer <clown@alum.mit.edu>
2018-07-31 10:16:53 -0400

    GPII-3138:  Docker image that updates snapset in the database
    Removed sensitive information from CouchDB url when logged to the
    console.

--- deleteAndLoadSnapsets.sh
+++ deleteAndLoadSnapsets.sh
@@ -23,8 +23,10 @@ if [ ! -d "$BUILD_DATA_DIR" -o ! "$(ls -A $BUILD_DATA_DIR/*.json)" ]; then
   exit 1
 fi
 
+COUCHDB_URL_SANITIZED=`echo "$COUCHDB_URL" | sed -e 's,\(://\)[^/]*\(@\),\1<SENSITIVE>\2,g'`
+
 log "Starting"
-log "CouchDB: $COUCHDB_URL"
+log "CouchDB: $COUCHDB_URL_SANITIZED"
 log "Clear index: $CLEAR_INDEX"
 log "Static: $STATIC_DATA_DIR"
 log "Build: $BUILD_DATA_DIR"
@@ -44,13 +46,13 @@ node scripts/convertPrefs.js testData/preferences/ build/dbData/snapset snapset
 
 # Initialize (possibly clear) data base
 if [ ! -z "$CLEAR_INDEX" ]; then
-  log "Deleting database at $COUCHDB_URL"
+  log "Deleting database at $COUCHDB_URL_SANITIZED"
   if ! curl -fsS -X DELETE "$COUCHDB_URL"; then
     log "Error deleting database"
   fi
 fi
 
-log "Creating database at $COUCHDB_URL"
+log "Creating database at $COUCHDB_URL_SANITIZED"
 if ! curl -fsS -X PUT "$COUCHDB_URL"; then
   log "Database already exists at $COUCHDB_URL"
 fi
