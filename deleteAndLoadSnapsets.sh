9bb3f46a907cb19399eedba1a75e9c0fa24c8973
Joseph Scheuhammer <clown@alum.mit.edu>
2018-07-31 16:37:06 -0400

    GPII-3138:  Docker image that updates snapset in the database
    Updated to handle demo 'user' Prefs Safes.

--- deleteAndLoadSnapsets.sh
+++ deleteAndLoadSnapsets.sh
@@ -2,6 +2,7 @@
 
 STATIC_DATA_DIR=${STATIC_DATA_DIR:-/home/node/universal/testData/dbData}
 BUILD_DATA_DIR=${BUILD_DATA_DIR:-/home/node/universal/build/dbData/snapset}
+BUILD_DEMOUSER_DIR=${BUILD_DEMOUSER_DIR:-/home/node/universal/build/dbData/demouser}
 
 log() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
@@ -23,6 +24,11 @@ if [ ! -d "$BUILD_DATA_DIR" -o ! "$(ls -A $BUILD_DATA_DIR/*.json)" ]; then
   exit 1
 fi
 
+if [ ! -d "$BUILD_DEMOUSER_DIR" -o ! "$(ls -A $BUILD_DEMOUSER_DIR/*.json)" ]; then
+  echo "BUILD_DATA_DIR ($BUILD_DEMOUSER_DIR) must exist and contain data"
+  exit 1
+fi
+
 COUCHDB_URL_SANITIZED=`echo "$COUCHDB_URL" | sed -e 's,\(://\)[^/]*\(@\),\1<SENSITIVE>\2,g'`
 
 log "Starting"
@@ -30,6 +36,7 @@ log "CouchDB: $COUCHDB_URL_SANITIZED"
 log "Clear index: $CLEAR_INDEX"
 log "Static: $STATIC_DATA_DIR"
 log "Build: $BUILD_DATA_DIR"
+log "Demo User: $BUILD_DEMOUSER_DIR"
 log "Working directory: `pwd`"
 
 # Set up universal
@@ -42,7 +49,8 @@ npm install rimraf
 npm install mkdirp
 npm install infusion
 rm -f package-lock.json
-node scripts/convertPrefs.js testData/preferences/ build/dbData/snapset snapset
+node scripts/convertPrefs.js testData/preferences/ build/dbData/snapset/ snapset
+node scripts/convertPrefs.js testData/preferences/demoUserPrefs/ build/dbData/demouser/ user
 
 # Initialize (possibly clear) data base
 if [ ! -z "$CLEAR_INDEX" ]; then
@@ -58,4 +66,4 @@ if ! curl -fsS -X PUT "$COUCHDB_URL"; then
 fi
 
 # Submit data
-node scripts/deleteAndLoadSnapsets.js $COUCHDB_URL $STATIC_DATA_DIR $BUILD_DATA_DIR
+node scripts/deleteAndLoadSnapsets.js $COUCHDB_URL $STATIC_DATA_DIR $BUILD_DATA_DIR $BUILD_DEMOUSER_DIR
